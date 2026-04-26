468d41b4883b826bfa9843a2b0e92b482ab5cad5
RaditAtalla <radityaderachmadie@gmail.com>
2026-04-27 03:43:54 +0700

    first commit

---
+++ CovidConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL9_103022430007
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu == "celcius")
+            {
+                satuan_suhu = "fahrenheit";
+            }
+            else if (satuan_suhu == "fahrenheit")
+            {
+                satuan_suhu = "celcius";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using TP_MODUL9_103022430007;
+
+var json = File.ReadAllText("covid_config.json");
+var options = new JsonSerializerOptions
+{
+    IncludeFields = true,
+};
+
+var config = JsonSerializer.Deserialize<CovidConfig>(json, options);
+
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_suhu);
+var suhu = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+var hari = int.Parse(Console.ReadLine());
+
+if (config.satuan_suhu == "celcius")
+{
+    Console.WriteLine((suhu >= 36.5 && suhu <= 37.5) && hari < config.batas_hari_deman
+                        ? config.pesan_diterima : config.pesan_ditolak);
+}
+else if (config.satuan_suhu == "fahrenheit")
+{
+    Console.WriteLine((suhu >= 97.7 && suhu <= 99.5) && hari < config.batas_hari_deman
+                        ? config.pesan_diterima : config.pesan_ditolak);
+}
+
+config.UbahSatuan();
+Console.WriteLine(config.satuan_suhu);
